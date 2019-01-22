0285a9268ff99da55793ea5509a6c1cab4a5fbfc
Fernando <ferch5003@gmail.com>
2019-01-22 15:01:28 -0500

    Contains the configuraion of the database settings

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+    MySQL: specify the mysql adapter.
+    PostgreSQL: specify the pgsql adapter.
+    SQLite: specify the sqlite adapter.
+    SQL Server: specify the sqlsrv adapter.
+*/
+define('DB_ADAPTER','mysql');
+define('DB_NAME','app');
+define('DB_HOST','localhost');
+define('DB_PORT',3306);
+define('DB_USER','root');
+define('DB_PASS','secret');
