a7866f26b0c87caadafd5643ec9f0c7749d9d854
Fernando <ferch5003@gmail.com>
2019-02-09 13:42:06 -0500

    Added users table name

--- Model/User.php
+++ Model/User.php
@@ -4,6 +4,9 @@ namespace Models;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model{
+
+    protected $table = 'users';
+
     protected $fillable = [
         'name', 'email', 'password'
     ];
